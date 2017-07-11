3b8c7302cf7735a44c2d86e32be5b918afb5dfed
Stoeffel <schtoeffel@gmail.com>
2017-07-11 08:53:57 +0200

    add simple test

--- tests/Example.elm
+++ tests/Example.elm
@@ -1,10 +1,15 @@
 module Example exposing (..)
 
 import Expect exposing (Expectation)
-import Fuzz exposing (Fuzzer, list, int, string)
+import Fuzz exposing (Fuzzer, int, list, string)
 import Test exposing (..)
 
 
 suite : Test
 suite =
-    todo "Implement our first test. See http://package.elm-lang.org/packages/elm-community/elm-test/latest for how to do this!"
+    describe "elm-package issue in elm-test"
+        [ test "simple" <|
+            \() ->
+                String.length "asdf"
+                    |> Expect.equal 4
+        ]
